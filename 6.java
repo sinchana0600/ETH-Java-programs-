59c5205189f26b576b02388ab747a5c984f99284
Sinchana Shankar <sinchana040607@gmail.com>
2026-02-11 10:30:23 +0530

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,16 @@
+package programs;
+
+class Animal {
+    String name;
+}
+
+public class prg1 {
+    public static void main(String[] args) {
+
+        Animal a1 = new Animal();
+        a1.name = "Dog";
+
+        System.out.println(a1.name);
+    }
+}
+
